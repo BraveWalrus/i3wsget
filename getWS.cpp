aff23774ab3a5c213ad201bfef6bd3d380897aa1
Bartek Wardega <wardegab@gmail.com>
2021-01-15 09:12:33 -0700

    getWS not instantiates two boolean arrays, one for a workspace's existence and the other for whether or not it is visable

--- getWS.cpp
+++ getWS.cpp
@@ -1,5 +1,6 @@
 #include <string>
 #include <iostream>
+#include <sstream>
 using namespace std;
 
 
@@ -9,7 +10,31 @@ string GetStdoutFromCommand(string cmd);
 int main() {
     string cmd = "i3-msg -t get_workspaces";
     string rawOut = GetStdoutFromCommand(cmd);
-    cout<<rawOut<<endl;
+    bool isReal[5] = {0}, isVisable[5] = {0};
+
+    for(int i=0; i<rawOut.length()-6; i++){
+      char window[3];
+      for(int j=0; j<3; j++){
+        window[j] = rawOut[i+j];
+      }
+      string windowStr(window);
+      if(windowStr=="num"){
+        int windInd = rawOut[i+5] - '0';
+        isReal[windInd] = 1;
+        if(rawOut[i+28]=='t'){
+          isVisable[windInd] = 1;
+        }
+      }
+    }
+    for(int i=0;i<5;i++){
+      cout<<isReal[i]<<" ";
+    }
+    cout<<endl;
+
+    for(int i=0;i<5;i++){
+      cout<<isVisable[i]<<" ";
+    }
+    cout<<endl;
     return 0;
 }
 
